e41af28ca69e4189c45e2b4e3746f1a801ba878d
José Sánchez Moreno <jose@o2w.es>
2012-05-09 10:32:19 +0200

    Instead of using local ip we check developer server ip

--- htdocs/app_dev.php
+++ htdocs/app_dev.php
@@ -6,12 +6,12 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        '127.0.0.1',
-        '::1',
-    ))
+    || !in_array(@$_SERVER['SERVER_ADDR'], array(
+                     '192.168.200.214'
+                 ))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
